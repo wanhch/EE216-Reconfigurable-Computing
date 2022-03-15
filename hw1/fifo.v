32dea343063add219192a50996d7ff4dea597671
Haochuan Wan <wanhch@shanghaitech.edu.cn>
2022-03-15 15:22:44 +0800

    finish hw1

--- hw1/fifo.v
+++ hw1/fifo.v
@@ -1,8 +1,62 @@
 module fifo (
-    input clk, 
-    input rst_n, 
-    input [7:0] buf_in, 
+    input            clk    , 
+    input            rst_n  , 
+    input            wr_en  , 
+    input            rd_en  ,
+    input [7:0]      buf_in , 
     output reg [7:0] buf_out, 
-    output empty, 
-    output full, 
+    output           empty  , 
+    output           full   , 
     output reg [3:0] fifo_cnt);
+
+reg [7:0] mem [0:7];
+reg [2:0] wp;
+reg [2:0] rp;
+
+always @(fifo_cnt) begin
+    if (fifo_cnt == 4'd0) 
+        empty <= 1'b1;
+    else 
+        empty <= 1'b0;
+    if (fifo_cnt == 4'd8)
+        full <= 1'b1;
+    else 
+        full <= 1'b0;
+end
+
+always @(posedge clk) begin
+    if (rst_n == 1'b1) begin
+        buf_out  <= 8'd0;
+        fifo_cnt <= 4'd0;
+        wp       <= 3'd0;
+        rp       <= 3'd0;
+    end else begin
+        if (wr_en == 1'b1) begin
+            if (full == 0'b0) begin
+                if (wp == 3'd7) begin
+                    wp <= 3'd0;
+                    mem[0] <= buf_in;
+                else begin 
+                    wp <= wp + 1;
+                    mem[wp + 1] <= buf_in;
+                end
+                fifo_cnt <= fifo_cnt + 1;
+            end
+        end
+
+        if (rd_en == 1'b1) begin
+            if (empty == 0'b0) begin
+                if (rp == 3'd7) begin
+                    rp <= 3'd0;
+                    buf_out <= mem[0];
+                end else begin
+                    rp <= rp + 1;
+                    buf_out <= mem[rp + 1];
+                end
+                fifo_cnt <= fifo_cnt - 1;
+            else begin
+                buf_out <= 8'd0;
+            end
+        end
+    end
+end
---
+++ hw1/fifo_tb.v
@@ -0,0 +1,74 @@
+`define BUF_WIDTH   4     //��ַ����Ϊ3+1��
+`define BUF_SIZE    (8)    //���ݸ�����FIFO���
+
+module tb_fifo;
+	reg clk,rst_n;
+	reg wr_en,rd_en;
+	reg [7:0] buf_in;		     // data input to be pushed to buffer
+	wire [7:0] buf_out;       // port to output the data using pop.
+    wire buf_empty,buf_full;  // buffer empty and full indication 
+	wire [`BUF_WIDTH-1:0] fifo_cnt;  // number of data pushed in to buffer 
+	
+	fifo dut(clk,rst_n,wr_en,rd_en,buf_in,buf_out,buf_empty,buf_full,fifo_cnt);
+	
+	always #10 clk = ~clk;
+	
+	initial begin
+		clk = 0;
+		rst_n = 0;
+		wr_en = 0;
+		rd_en = 0;
+		buf_in = 0;
+		#15;
+		rst_n = 1;
+		
+		push(1);
+		fork
+			push(2);
+			pop();
+		join			//push and pop together   
+		push(10);
+		push(20);
+		push(30);
+		push(40);
+		push(50);
+		push(60);
+		push(70);
+		pop();
+		push(2);
+		pop();
+		pop();
+		pop();
+		pop();
+		push(140);
+		pop();
+		push(50);//
+		pop();
+		pop();
+		pop();
+		pop();
+		pop();
+		push(140);
+		pop();
+		
+	end
+	
+	task push (input [7:0] data);
+	begin
+			buf_in = data;
+			wr_en = 1;
+			@(posedge clk);
+			#5 wr_en = 0;
+		end
+	endtask
+	
+	task pop();
+	begin
+			rd_en = 1;
+			@(posedge clk);
+			#3 rd_en = 0;
+	end
+	endtask
+	
+	
+endmodule 
